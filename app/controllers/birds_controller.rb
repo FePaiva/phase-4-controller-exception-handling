a098c6c695773950d799742de6a2d5a9c51288a9
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-02-01 16:01:21 -0800

    commit tree 33f94272a4ea7ea950ac2bf3b860a1886de683da

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /birds
   def index
@@ -14,51 +15,57 @@ class BirdsController < ApplicationController
 
   # GET /birds/:id
   def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    render json: bird
+
+    # removing the 2 lines below bc since the rescue runs in any ofthe errors, we do not need the error handling.
+    # rescue ActiveRecord::RecordNotFound
+    # render_not_found_response
   end
 
   # PATCH /birds/:id
   def update
-    bird = Bird.find_by(id: params[:id])
-    if bird
+    bird = find_bird
       bird.update(bird_params)
       render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    # removing the 2 lines below bc since the rescue runs in any ofthe errors, we do not need the error handling.
+    # rescue ActiveRecord::RecordNotFound
+    # render_not_found_response
   end
 
   # PATCH /birds/:id/like
   def increment_likes
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(likes: bird.likes + 1)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.update(likes: bird.likes + 1)
+    render json: bird
+   # removing the 2 lines below bc since the rescue runs in any ofthe errors, we do not need the error handling.
+    # rescue ActiveRecord::RecordNotFound
+    # render_not_found_response
+       
   end
 
   # DELETE /birds/:id
   def destroy
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.destroy
-      head :no_content
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.destroy
+    head :no_content
+   # removing the 2 lines below bc since the rescue runs in any ofthe errors, we do not need the error handling.
+    # rescue ActiveRecord::RecordNotFound
+    # render_not_found_response
   end
 
   private
 
   def bird_params
     params.permit(:name, :species, :likes)
+  end
+
+  def find_bird 
+    Bird.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
   end
 
 end
